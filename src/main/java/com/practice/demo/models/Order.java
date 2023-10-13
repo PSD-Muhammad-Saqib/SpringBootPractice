2fd4f7ad91335e58d50c50094be342e4fc77eab5
PSD-Muhammad-Saqib <saqib.saif@netsoltech.com>
2023-10-13 09:42:46 +0500

    [main] one to one mapping

--- src/main/java/com/practice/demo/models/Order.java
+++ src/main/java/com/practice/demo/models/Order.java
@@ -1,16 +1,69 @@
 package com.practice.demo.models;
 
+import java.util.Date;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "orders")
 public class Order {
-//	OrderID	CustomerID	EmployeeID	OrderDate	ShipperID
+//	OrderID	CustomerID	EmployeeID	OrderDate	ShipperID	OrderDetailID
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@Column(nullable = true)
+	private Date orderDate;
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = true)
+	@JoinColumn(name = "order_detail_id", nullable = true, referencedColumnName = "id")
+	private OrderDetail orderDetail;
+
+	public Order() {
+	}
+
+	public Order(Integer id, Date orderDate, OrderDetail orderDetail) {
+		this.id = id;
+		this.orderDate = orderDate;
+		this.orderDetail = orderDetail;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", orderDate=" + orderDate + ", orderDetail=" + orderDetail + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderDetail getOrderDetail() {
+		return orderDetail;
+	}
+
+	public void setOrderDetail(OrderDetail orderDetail) {
+		this.orderDetail = orderDetail;
+	}
+
 }
--- src/main/java/com/practice/demo/models/OrderDetail.java
+++ src/main/java/com/practice/demo/models/OrderDetail.java
@@ -1,9 +1,13 @@
 package com.practice.demo.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,4 +18,61 @@ public class OrderDetail {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	private Integer quantity;
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "product_id", nullable = false, referencedColumnName = "id")
+	private Product product;
+
+	@OneToOne(fetch = FetchType.LAZY, optional = false, mappedBy = "orderDetail")
+	private Order order;
+
+	public OrderDetail() {
+	}
+
+	public OrderDetail(Integer id, Integer quantity, Product product, Order order) {
+		this.id = id;
+		this.quantity = quantity;
+		this.product = product;
+		this.order = order;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderDetail [id=" + id + ", quantity=" + quantity + ", product=" + product + ", order=" + order + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
 }
